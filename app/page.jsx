6933d90ceab9f925d17d5a7cc206f8bf212dfb9f
Roza Russkikh <rrrusskikh@gmail.com>
2023-05-29 18:28:29 -0700

    Set up

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import "@styles/globals.css";
+
+const page = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Share & Find</h1>
+      <br className="max-md:hidden"/>
+      <span className="orange_gradient text-center">Your favorite books</span>
+      <p>This is a Book Board to swap books and enjoy reading</p>
+    </section>
+  );
+};
+
+export default page;
